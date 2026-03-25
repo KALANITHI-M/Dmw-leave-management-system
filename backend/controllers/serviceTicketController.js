c3acfa141997a6ec0d01bc82cd7e66f03252d767
KALANITHI M <kalanithi54321@gmail.com>
2026-03-25 21:51:57 +0530

    Service ticket backend issue

--- backend/controllers/serviceTicketController.js
+++ backend/controllers/serviceTicketController.js
@@ -6,6 +6,12 @@ import { v2 as cloudinary } from 'cloudinary';
 import multer from 'multer';
 import path from 'path';
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 // File storage setup
 const memStorage = multer.memoryStorage();
 
